0401b9dc6e698ce3b5c6e7e12537ea8b3e496069
willsztej <williamsztejnberg@gmail.com>
2020-09-28 15:17:20 -0400

    Connected backend to MySQL database

--- backend/StockTalk/settings.py
+++ backend/StockTalk/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'StockTalk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': 'WillSztej',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
