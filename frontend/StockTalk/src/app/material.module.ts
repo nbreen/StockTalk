13bea2a918acc111f26d1338ce05cb68affc2516
willsztej <williamsztejnberg@gmail.com>
2020-09-30 18:46:07 -0400

    Implemented simple UI for login and signup pages

--- frontend/StockTalk/src/app/material.module.ts
+++ frontend/StockTalk/src/app/material.module.ts
@@ -2,11 +2,50 @@ import { NgModule } from  '@angular/core';
  
 import { MatButtonModule } from  '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatListModule } from  '@angular/material/list';
+import { MatDatepickerModule}  from  '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+ 
  
  
 @NgModule({
-imports: [MatButtonModule,MatToolbarModule],
-exports: [MatButtonModule,MatToolbarModule],
+imports: [MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatListModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule
+],
+exports: [MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatListModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule
+],
  
 })
  
