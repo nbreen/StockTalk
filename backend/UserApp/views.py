4553745146a310709ecb7365e5efca43e6757a68
sriniv72 <43009951+sriniv72@users.noreply.github.com>
2020-11-02 14:51:02 +0530

    Update views.py
    Updating followTopicApi

--- backend/UserApp/views.py
+++ backend/UserApp/views.py
@@ -6,6 +6,7 @@ from django.db import connection
 
 from UserApp.models import Users
 from UserApp.serializers import UserSerializer
+from UserApp.serializers import FollowTopicSerializer
 import re
 
 # Create your views here.
@@ -57,31 +58,22 @@ def userApi(request,id=0):
         return JsonResponse("Deleted user sucessfully", safe=False)    
     
 @csrf_exempt    
-def followTopicApi(request):
+def followTopicApi(request, id, fol):
     if request.method=='GET':
-        user = Users.objects.all()
-        user_serializer = UserSerializer(user, many=True)
-        return JsonResponse(user_serializer.data, safe=False)
+        value = UserFollowsTopic.objects.all()
+        follow_serializer = FollowTopicSerializer(value, many=True)
+        return JsonResponse(follow_serializer.data, safe=False)
 
     elif request.method=='POST':
-        user_data = JSONParser().parse(request)
-        user_serializer = UserSerializer(data=user_data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse("User added successfully", safe=False)
-        print(user_serializer.errors)
-        return JsonResponse("Failed to add user", safe=False)
-
-    elif request.method=='PUT':
-        user_data = JSONParser().parse(request)
-        user = Users.objects.get(UserID=user_data['UserID'])
-        user_serializer = UserSerializer(user, data=user_data) 
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse("User updated successfully", safe=False)
-        return JsonResponse("Failed to update user", safe=False)
+        follow_data = JSONParser().parse(request)
+        follow_serializer = FollowTopicSerializer(data=follow_data)
+        if follow_serializer.is_valid():
+            follow_serializer.save()
+            return JsonResponse("User followed successfully", safe=False)
+        print(follow_serializer.errors)
+        return JsonResponse("Failed to follow user", safe=False)
 
     elif request.method=='DELETE':
-        user=Users.objects.get(UserID=id)
-        user.delete()
-        return JsonResponse("Deleted user sucessfully", safe=False)
+        value=UserFollowsTopic.objects.get(UserID=id, TopicFollowed=fol)
+        value.delete()
+        return JsonResponse("User unfollowed sucessfully", safe=False)
