7c4124b096d123042cee49f99586be0e60a28690
Sithu <saung@purdue.edu>
2020-11-04 14:57:11 -0500

    deletes double url patterns + conflicting ','

--- backend/ProfileApp/urls.py
+++ backend/ProfileApp/urls.py
@@ -15,8 +15,5 @@ urlpatterns=[
     url(r'followuser/.*$', views.followuser),
     url(r'unfollowuser/.*$', views.unfollowuser),
     url(r'^post/.$', views.getAllPosts)
-    url(r'unfollowuser/.*$', views.unfollowuser),
-    url(r'followuser/.*$', views.followuser)
-
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
