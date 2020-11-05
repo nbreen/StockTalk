db9b62ee3e1571284cc1214326cfcf5dbe2cc7bf
Sithu <saung@purdue.edu>
2020-11-04 21:16:29 -0500

    Can use serializers to send posts to the frontend successfuly

--- backend/PostApp/models.py
+++ backend/PostApp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    PostId = models.IntegerField(default=0)
-    UserID = models.IntegerField(default=0)
+    Username = models.CharField(default='', max_length=256)
+    PostId = models.AutoField(primary_key=True)
+    UserId = models.IntegerField(default=0)
     TopicName = models.CharField(max_length=256, default='')
     PostType = models.IntegerField(default=0)
     Post = models.CharField(max_length=256, default='')
@@ -12,4 +13,4 @@ class Post(models.Model):
     Downvotes = models.IntegerField(default=0)
     Upvotes = models.IntegerField(default=0)
     Anonymous = models.IntegerField(default=0)
-    PostImage = models.CharField(max_length=256, default='empty')
+    PostImage = models.CharField(default='',max_length=256)
