dbdc4a0d3873e4c8962ceaf867c0603a4f4e746e
Sithu <saung@purdue.edu>
2020-11-05 12:03:42 -0500

    Makes displaying posts work with the new Django PostsApp

--- backend/PostsApp/urls.py
+++ backend/PostsApp/urls.py
@@ -5,5 +5,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^addPosts/$', views.postApi)
+    url(r'^addPosts/$', views.postApi),
+    url(r'^getPost/BySaved/.*$', views.getSavedPost),
+    url(r'^getPost\/.*$', views.getPost),
+    url(r'^checksavedpost\/.*$', views.checkSavedPost),
+    url(r'^unsavepost\/.*$', views.unsavePost),
+    url(r'^savepost\/.*$', views.savePost),
+    url(r'^getAllPosts\/.*$', views.getAllPosts),
+    url(r'^addPost/$', views.postApi)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/PostsApp/views.py
+++ backend/PostsApp/views.py
@@ -16,6 +16,114 @@ from django.db import connection
 
 # Create your views here.
 @csrf_exempt
+def getSavedPost(Method):
+    # request = (str(Method)).split("/")
+    # Username = (str(request[3]))[:-2]
+    # print(Username)
+    # post = Post.objects.filter(Username=Username)
+
+    # post_serializer = PostSerializer(post, many=True)
+    # print(post_serializer)
+    # return JsonResponse(post_serializer.data, safe=False)
+
+
+    request = (str(Method)).split("/")
+    Username = (str(request[3]))[:-2]
+    print(Username)
+    query = f'SELECT PostId FROM UserSavesPost WHERE Username = "{Username}";'
+    cursor = connection.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    print(records)
+    cursor.close()
+    # post_serializer = PostSerializer(records, many=True)
+    # print(post_serializer)
+    return JsonResponse(records, safe=False)
+
+
+# Create your views here.
+def getAllPosts(Method):
+    post = Posts.objects.all()
+    print(post)
+    posts_serializer = PostsSerializer(post, many=True)
+    print(posts_serializer)
+    return JsonResponse(posts_serializer.data, safe=False)
+
+@csrf_exempt
+def getPost(Method):
+    print(str(Method))
+    request = (str(Method)).split("/")
+    filterBy = request[2]
+    mustEqual = (request[3])[:-2]
+    #print()
+    #print(filterBy)
+    #print(mustEqual)
+
+    post = "Hello"
+    if (filterBy == "ByTopic"):
+        post = Posts.objects.all().filter(TopicName=mustEqual)
+        print(post)
+    elif (filterBy == "ByUser"):
+        post = Posts.objects.all().filter(Username=mustEqual)
+
+
+    # post = Post.objects.all().filter(TopicName=topicname)
+
+    print(post)
+    posts_serializer = PostsSerializer(post, many=True)
+    print(posts_serializer)
+    return JsonResponse(posts_serializer.data, safe=False)
+
+
+@csrf_exempt
+def checkSavedPost(Method):
+    print(str(Method))
+    request = (str(Method)).split("/")
+    Username = request[2]
+    PostId = (request[3])[:-2]
+
+    query = f'SELECT * FROM UserSavesPost WHERE Username = "{Username}" AND PostId = "{PostId}";'
+    cursor = connection.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+
+    print(records)
+    cursor.close()
+    return JsonResponse(records, safe=False)
+
+@csrf_exempt
+def savePost(request):
+    name = str(request).split("/")
+    Username = name[2]
+    PostId = (name[3])[:-2]
+    #print(Username)
+    #print(PostId)
+
+    query = f'INSERT INTO UserSavesPost VALUES(\"{Username}\", \"{PostId}\");'
+    cursor = connection.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    # print(records)
+    cursor.close()
+    return JsonResponse(records, safe=False)
+
+@csrf_exempt
+def unsavePost(request):
+    name = str(request).split("/")
+    Username = name[2]
+    PostId = (name[3])[:-2]
+    #print(Username)
+    #print(PostId)
+
+    query = f'SET SQL_SAFE_UPDATES = 0; DELETE FROM UserSavesPost WHERE Username = \"{Username}\" AND PostId = \"{PostId}\"; SET SQL_SAFE_UPDATES = 1;'
+    cursor = connection.cursor()
+    cursor.execute(query)
+    #connection.commit()
+    cursor.close()
+    return JsonResponse("success", safe=False)
+
+
+@csrf_exempt
 def postApi(request, id=0):
 
 
